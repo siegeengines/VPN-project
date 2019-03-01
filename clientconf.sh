21c5e4b367fcae6c119dc57a21f0e5a11df9205e
siegeengines <46577278+siegeengines@users.noreply.github.com>
2019-03-01 16:54:59 -0500

    Add files via upload

--- clientconf.sh
+++ clientconf.sh
@@ -11,26 +11,26 @@ echo '{
   },
   "inbounds": [
     {
-      "port": 10808, // 监听端口
-      "protocol": "socks", // 入口协议为 SOCKS 5
+      "port": 10808, 
+      "protocol": "socks", 
       "domainOverride": ["tls","http"],
       "settings": {
-        "auth": "noauth"  //socks的认证设置，noauth 代表不认证，由于 socks 通常在客户端使用，所以这里不认证
+        "auth": "noauth" 
       }
     }
   ],
   "outbounds": [
     {
-      "protocol": "vmess", // 出口协议
+      "protocol": "vmess", 
       "settings": {
         "vnext": [
 		  {
-            "address": "m1.shebao.gq", // 服务器地址，请修改为你自己的服务器 IP 或域名
-            "port": 443,  // 服务器端口
+            "address": "m1.shebao.gq",
+            "port": 443, 
             "users": [
               {
-                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91",  // 用户 ID，必须与服务器端配置相同
-                "alterId": 64, // 此处的值也应当与服务器相同
+                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91", 
+                "alterId": 64, 
 				"security": "auto"
               }
             ]
@@ -60,14 +60,14 @@ echo '{
       {
         "type": "field",
         "outboundTag": "direct",
-        "domain": ["geosite:cn"] // 中国大陆主流网站的域名
+        "domain": ["geosite:cn"] 
       },
       {
         "type": "chinaip",
         "outboundTag": "direct",
         "ip": [
-          "geoip:cn",// 中国大陆的 IP
-          "geoip:private" // 私有地址 IP，如路由器等
+          "geoip:cn",
+          "geoip:private" 
         ]
       }
     ]
@@ -84,48 +84,48 @@ echo '{
   },
   "inbounds": [
     {
-      "port": 10808, // 监听端口
-      "protocol": "socks", // 入口协议为 SOCKS 5
+      "port": 10808, 
+      "protocol": "socks",
       "domainOverride": ["tls","http"],
       "settings": {
-        "auth": "noauth"  //socks的认证设置，noauth 代表不认证，由于 socks 通常在客户端使用，所以这里不认证
+        "auth": "noauth"  
       }
     }
   ],
   "outbounds": [
     {
-      "protocol": "vmess", // 出口协议
+      "protocol": "vmess", 
       "settings": {
         "vnext": [
           {
-            "address": "s1.shebao.gq", // 服务器地址，请修改为你自己的服务器 IP 或域名
-            "port": 443,  // 服务器端口
+            "address": "s1.shebao.gq", 
+            "port": 443, 
             "users": [
               {
-                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91",  // 用户 ID，必须与服务器端配置相同
-                "alterId": 64, // 此处的值也应当与服务器相同
+                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91", 
+                "alterId": 64, 
 				"security": "auto"
               }
             ]
           },
 		  {
-            "address": "s2.shebao.gq", // 服务器地址，请修改为你自己的服务器 IP 或域名
-            "port": 443,  // 服务器端口
+            "address": "s2.shebao.gq", 
+            "port": 443, 
             "users": [
               {
-                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91",  // 用户 ID，必须与服务器端配置相同
-                "alterId": 64, // 此处的值也应当与服务器相同
+                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91", 
+                "alterId": 64,
 				"security": "auto"
               }
             ]
           },
 		  {
-            "address": "s3.shebao.gq", // 服务器地址，请修改为你自己的服务器 IP 或域名
-            "port": 443,  // 服务器端口
+            "address": "s3.shebao.gq",
+            "port": 443, 
             "users": [
               {
-                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91",  // 用户 ID，必须与服务器端配置相同
-                "alterId": 64, // 此处的值也应当与服务器相同
+                "id": "a2d029c7-ee40-42bd-b526-69ddbaf8ea91",  
+                "alterId": 64, 
 				"security": "auto"
               }
             ]
@@ -155,14 +155,14 @@ echo '{
       {
         "type": "field",
         "outboundTag": "direct",
-        "domain": ["geosite:cn"] // 中国大陆主流网站的域名
+        "domain": ["geosite:cn"] 
       },
       {
         "type": "chinaip",
         "outboundTag": "direct",
         "ip": [
-          "geoip:cn",// 中国大陆的 IP
-          "geoip:private" // 私有地址 IP，如路由器等
+          "geoip:cn",
+          "geoip:private" 
         ]
       }
     ]
